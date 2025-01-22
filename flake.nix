afafa4c4608539eb7206ae9dbb1611b60a282f2b
h0m1 <h0m1@proton.me>
2025-01-22 16:57:51 +0100

    added formatter to flake.nix

--- flake.nix
+++ flake.nix
@@ -7,52 +7,60 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-    rust-overlay,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {inherit system overlays;};
-      rust = pkgs.rust-bin.stable.latest.default.override {
-        extensions = ["rust-src"];
-      };
-      rustPlatform = pkgs.makeRustPlatform {
-        rustc = rust;
-        cargo = rust;
-      };
-    in {
-      packages = rec {
-        mqtt2timescale = rustPlatform.buildRustPackage rec {
-          pname = "mqtt2timescale";
-          version = "0.1.0";
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+      in
+      {
+        packages = rec {
+          mqtt2timescale = rustPlatform.buildRustPackage rec {
+            pname = "mqtt2timescale";
+            version = "0.1.0";
 
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            doCheck = true;
+
+            env = { };
           };
 
-          doCheck = true;
-
-          env = {};
+          default = mqtt2timescale;
         };
-
-        default = mqtt2timescale;
-      };
-      apps = rec {
-        ip = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.mqtt2timescale;
-          exePath = "/bin/mqtt2timescale";
+        apps = rec {
+          ip = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.mqtt2timescale;
+            exePath = "/bin/mqtt2timescale";
+          };
+          default = ip;
         };
-        default = ip;
-      };
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [rust];
-        shellHook = ''
-          test -f ./env.sh && . ./env.sh
-        '';
-      };
-    });
+        formatter = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust
+          ];
+          shellHook = ''
+            test -f ./env.sh && . ./env.sh
+          '';
+        };
+      }
+    );
 }
